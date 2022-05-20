5a8e4cc9fc2146887dcd4ff7cd683ef3e2c18c7e
CristianCamiloRT <cristianramirez.desarrollador@hotmail.com>
2022-05-20 17:55:51 -0500

    Ajustes en el leftbar

--- templates/element/sliderbar.php
+++ templates/element/sliderbar.php
@@ -2,7 +2,12 @@
     <div class="sidebar_blog_1">
         <div class="sidebar-header">
             <div class="logo_section">
-                <a href="index.html"><img class="logo_icon img-responsive" src="./img/logo_icon.png" alt="#" /></a>
+                <?= $this->Html->image('Img_IconoPestana3.png', [
+                        'alt' => 'Logo Pantera',
+                        'url' => ['controller' => 'Users', 'action' => 'index'],
+                        'class' => 'logo_icon img-responsive'
+                    ]);
+                ?>
             </div>
         </div>
         <div class="sidebar_user_info">
@@ -29,7 +34,7 @@
                 <a href="#dashboard" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-users yellow_color"></i> <span>Usuarios</span></a>
                 <ul class="collapse list-unstyled" id="dashboard">
                 <li>
-                    <?= $this->Html->link(__('> <span>Usuarios</span>'),
+                    <?= $this->Html->link(__('> <span>Lista de Usuarios</span>'),
                         [
                             'controller' => 'Users',
                             'action' => 'index'
@@ -52,48 +57,34 @@
                 </li>
                 </ul>
             </li>
-            <!-- <li><a href="widgets.html"><i class="fa fa-clock-o orange_color"></i> <span>Widgets</span></a></li>
             <li>
-                <a href="#element" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-diamond purple_color"></i> <span>Elements</span></a>
-                <ul class="collapse list-unstyled" id="element">
-                <li><a href="general_elements.html">> <span>General Elements</span></a></li>
-                <li><a href="media_gallery.html">> <span>Media Gallery</span></a></li>
-                <li><a href="icons.html">> <span>Icons</span></a></li>
-                <li><a href="invoice.html">> <span>Invoice</span></a></li>
-                </ul>
-            </li>
-            <li><a href="tables.html"><i class="fa fa-table purple_color2"></i> <span>Tables</span></a></li>
-            <li>
-                <a href="#apps" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-object-group blue2_color"></i> <span>Apps</span></a>
-                <ul class="collapse list-unstyled" id="apps">
-                <li><a href="email.html">> <span>Email</span></a></li>
-                <li><a href="calendar.html">> <span>Calendar</span></a></li>
-                <li><a href="media_gallery.html">> <span>Media Gallery</span></a></li>
-                </ul>
-            </li>
-            <li><a href="price.html"><i class="fa fa-briefcase blue1_color"></i> <span>Pricing Tables</span></a></li>
-            <li>
-                <a href="contact.html">
-                <i class="fa fa-paper-plane red_color"></i> <span>Contact</span></a>
-            </li>
-            <li class="active">
-                <a href="#additional_page" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-clone yellow_color"></i> <span>Additional Pages</span></a>
-                <ul class="collapse list-unstyled" id="additional_page">
+                <a href="#Exercises" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-star purple_color"></i> <span>Ejercicios</span></a>
+                <ul class="collapse list-unstyled" id="Exercises">
                 <li>
-                    <a href="profile.html">> <span>Profile</span></a>
+                    <?= $this->Html->link(__('> <span>Lista de Ejercicios</span>'),
+                        [
+                            'controller' => 'Exercises',
+                            'action' => 'index'
+                        ],
+                        [
+                            'escape' => false
+                        ])
+                    ?>
                 </li>
                 <li>
-                    <a href="project.html">> <span>Projects</span></a>
-                </li>
-                <li>
-                    <a href="login.html">> <span>Login</span></a>
-                </li>
-                <li>
-                    <a href="404_error.html">> <span>404 Error</span></a>
+                    <?= $this->Html->link(__('> <span>Crear Ejercicio</span>'),
+                        [
+                            'controller' => 'Exercises',
+                            'action' => 'add'
+                        ],
+                        [
+                            'escape' => false
+                        ])
+                    ?>
                 </li>
                 </ul>
             </li>
-            <li><a href="map.html"><i class="fa fa-map purple_color2"></i> <span>Map</span></a></li>
+            <!-- <li><a href="map.html"><i class="fa fa-map purple_color2"></i> <span>Map</span></a></li>
             <li><a href="charts.html"><i class="fa fa-bar-chart-o green_color"></i> <span>Charts</span></a></li>
             <li><a href="settings.html"><i class="fa fa-cog yellow_color"></i> <span>Settings</span></a></li> -->
         </ul>
