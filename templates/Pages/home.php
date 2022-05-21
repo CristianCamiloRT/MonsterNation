822ea34d46864a12907617e733e1ac611ce806cf
CristianCamiloRT <cristianramirez.desarrollador@hotmail.com>
2022-05-20 19:57:05 -0500

    Ajustes home

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -1,78 +1,134 @@
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#"><img src="MaterialGrafico/img_logo.png" class="w-50 d-block d-md-none"></a>
         <a class="navbar-brand" href="#"><img src="MaterialGrafico/img_logo.png" class="w-100 d-none d-md-block"></a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Quienes Somos</a>
+                    <a class="nav-link" href="#somosMonster">Quienes Somos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Sedes</a>
+                    <a class="nav-link" href="#sedes">Sedes</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Planes</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="./users">Monster</a>
+                    <a class="nav-link" href="#nuestros-programas">Planes</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Contactos</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/users">Monster</a>
+                </li>
+
             </ul>
         </div>
     </nav>
 
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <!-- <img class="d-block w-100" src="..." alt="First slide"> -->
-                <video muted autoplay loop>
-                    <source src="MaterialGrafico/Vid_banner3.mp4" type="video/mp4">
-                </video>
-            </div>
-            <div>
-                <p>SOMOS MONSTER NATION</p>
+    <section id="hero">
+
+        <div class="banner">
+            <h1>SOMOS MONSTER NATION</h1>
+        </div>
+        <div class="overlay"></div>
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <!-- <img class="d-block w-100" src="..." alt="First slide"> -->
+                    <video muted autoplay loop>
+                        <source src="MaterialGrafico/Vid_banner3.mp4" type="video/mp4">
+                    </video>
+                </div>
             </div>
         </div>
-    </div>
+
+    </section>
 
     <section id="somosMonster">
         <div class="container">
             <div class="img-container"></div>
-            <h2>Somos Monster Nation</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim ut molestiae suscipit quibusdam maiores ipsa deleniti, obcaecati aspernatur voluptatem quod, aliquid doloribus saepe optio quae asperiores necessitatibus ea molestias numquam!</p>
+            <div class="texto">
+                <h2>Somos <span class="color-acento">Monster Nation</span></h2>
+                <p>Monster Nation es un proyecto que apoyara a los gimnasios con el control de sus trabajadores y
+                    diferentes clientes, a su vez facilitara la interacción entre entrenadores y clientes, también
+                    brindara un mayor orden al momento de realizar agendas de espacios de una manera digital, ya que
+                    muchos de estos procesos e información actualmente no son manejados de manera digital se generan
+                    ciertos eventos no ideales en las compañías.</p>
+            </div>
         </div>
     </section>
+
+    <section id="sedes">
+        <div class="container">
+            <h2>Sedes Monster</h2>
+
+            <div class="row">
+                <div class="container-card col-sm-12 col-md-6">
+                    <div class="card">
+                        <figure>
+                            <img src="MaterialGrafico/Img_BufaloGYM.jpg">
+                        </figure>
+                        <div class="contenido-card">
+                            <h2>BUFALO GYM</h2>
+                            <h3>Calle 47 sur #788</h3>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="maps col-sm-12 col-md-6 py-5">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d248.55469775333148!2d-74.16687176324075!3d4.616349098272915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9e891fbf4361%3A0x49e5589cd85ce845!2sB%C3%BAfalo%20Gym!5e0!3m2!1ses!2sco!4v1652993071998!5m2!1ses!2sco"
+                        width="600" height="450" style="border: 10px;;" allowfullscreen="" loading="lazy" class="mapa"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
 
 
     <section id="nuestros-programas">
         <div class="container">
             <h2>Nuestros Planes</h2>
 
-            <div class="programas">
+            <div class="programas row">
 
-                <div class="carta">
-                    <h3>Entrenamiento Oso</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque consectetur cum laborum ipsum beatae ad earum fugit voluptate unde, necessitatibus ex ducimus deserunt numquam ratione rem minus ut dolore maiores.</p>
-                    <button>+info</button>
+                <div class="carta col-sm-12 col-md-3">
+                    <img class="w-100" src="MaterialGrafico/Img_panda.jpg" >
+                    <h3 class="py-2">Entrenamiento Panda</h3>
+                    <p></p>
+                    <p class="px-3">1 mes de servicio, 2 a 3 sesiones de videollamada, plan de entrenamiento único orientado a tus
+                        objetivos, seguimiento en línea, entrenamientos y asesoría en línea de 45min que puedes hacer en
+                        cualquier lugar con la guía de tu entrenador y entrenamiento único orientado a tus objetivos.
+                    </p>
+                    <button><a id="abrir">+info</a></button>
                 </div>
 
-                <div class="carta">
+                <div class="carta col-sm-12 col-md-3">
+                    <img class="w-100" src="MaterialGrafico/Img_pantera.jpg" >
                     <h3>Entrenamiento Pantera</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque consectetur cum laborum ipsum beatae ad earum fugit voluptate unde, necessitatibus ex ducimus deserunt numquam ratione rem minus ut dolore maiores.</p>
+                    <p></p>
+                    <p class="px-3">3 mes de servicio, 5 a 10 sesiones de videollamada, plan de entrenamiento único orientado a tus
+                        objetivos, seguimiento en línea, entrenamientos y asesoría en línea de 90min que puedes hacer en
+                        cualquier lugar con la guía de tu entrenador y entrenamiento único orientado a tus objetivos,
+                        clases adicionales.</p>
                     <button>+info</button>
                 </div>
 
-
-                <div class="carta">
-                    <h3>Entrenamiento Gorilla</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque consectetur cum laborum ipsum beatae ad earum fugit voluptate unde, necessitatibus ex ducimus deserunt numquam ratione rem minus ut dolore maiores.</p>
+                <div class="carta col-sm-12 col-md-3">
+                    <img class="w-100" src="MaterialGrafico/Img_tiburon.jpg" >
+                    <h3>Entrenamiento Tiburon</h3>
+                    <p></p>
+                    <p class="px-3">6 mes de servicio, sesiones de videollamada ilimitadas, plan de entrenamiento único orientado a
+                        tus objetivos, seguimiento en línea, entrenamientos y asesoría en línea de puedes hacer en
+                        cualquier lugar con la guía de tu entrenador y entrenamiento único orientado a tus objetivos,
+                        clases adicionales y kit Monster Nation..</p>
                     <button>+info</button>
                 </div>
 
@@ -82,18 +138,15 @@
     </section>
 
 
-    <section id="sedes">
-
-
     </section>
 
     <section id="aplicaya">
-        <h2>INSCRIBETE YA</h2>
+        <h2>CONTACTANOS YA</h2>
         <button>Inscribete aqui</button>
 
     </section>
 
-
+    <footer>
         <footer class="footer">
             <div class="grupo-1">
                 <div class="box">
@@ -110,7 +163,7 @@
                 <div class="box">
                     <h2>SIGUENOS</h2>
                     <div class="red-social">
-                        <a href="#" class="facebook"></a>
+                        <a href="#" class="fa fa-facebook"></a>
                         <a href="#" class="instagram"></a>
                         <a href="#" class="twitter"></a>
                         <a href="#" class="youtube"></a>
@@ -123,4 +176,27 @@
             </div>
         </footer>
 
+    </footer>
 
+    <div id="miModal" class="modal">
+        <div class="flex" id="flex">
+            <div class="contenido-modal">
+                <div class="modal-header flex">
+                    <h2>NUESTROS PLANES</h2>
+                    <span class="close" id="close">&times;</span>
+                </div>
+                <div class="modal-body">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam voluptatum illum temporibus
+                        voluptas debitis! Et labore minus praesentium consequuntur mollitia dolores perferendis,
+                        voluptas nemo ratione consequatur illum quidem rerum,
+                        saepe eveniet ullam eaque quasi neque quo quisquam impedit ducimus voluptatem illo. Quod tenetur
+                        aliquam, soluta labore ipsam delectus. Id iusto distinctio minima quaerat nobis asperiores
+                        ullam, mollitia illo soluta quisquam natus
+                        dicta sint voluptates molestiae! Perferendis quos ea assumenda nulla?</p>
+                </div>
+                <div class="footer">
+                    <h3>.</h3>
+                </div>
+            </div>
+        </div>
+    </div>
